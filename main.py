d89401d8910d2051f8d0b06e0355a7c44ced7905
Krzysztof Kaniewski <krzysztof.kaniewski@gmail.com>
2023-03-09 15:25:25 +0100

    changed - simplified, function no longer related with test

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
 import datetime
 
-palindrome_test = [
+data_no_exception = [
     ('kajak', True),
     ('wielbłąd', False),
     ('Kajak', True),
-    ('0kajak0', False),
-    ([0, "k", 7, 3], False),
-    (datetime.date.today(), False),
-    (7, False),
-    (7.002, False)
+]
+
+data_raises_exception = [
+    ([0, "k", 7, 3], "Wrong data type"),
+    (datetime.date.today(), "Wrong data type"),
+    (7, "Wrong data type"),
+    (7.002, "Wrong data type"),
+    ("0kajak0", "Expected word!"),
+    ("kk", "String too short, needs to be at least 3 letters")
 ]
 
 
 def is_palindrome(data):
-    # data type check
-    if not isinstance(data[0], str):
-        raise ValueError(f'Wrong data type, provided {data[0]} - {data[1]}')
-    if not data[0].isalpha():
-        raise ValueError(f'Expected word, got - {data[0]} - {data[1]}')
-    if not len(data[0]) > 2:
-        raise ValueError(f'String too short, need to be at least 3 letters - {data[1]}')
-    if data[0].upper() == data[0].upper()[::-1]:
-        return data[1]
-    else:
-        return data[1]
+    if not isinstance(data, str):
+        raise ValueError('Wrong data type')
+    if not data.isalpha():
+        raise ValueError('Expected word!')
+    if not len(data) > 2:
+        raise ValueError('String too short, needs to be at least 3 letters')
+
+    data = data.upper()
+    return data == data[::-1]
 
 
-def test_palindrome():
-    try:
-        for x in palindrome_test:
-            is_palindrome(x)
-        assert False, "ValueError expected"
-    except ValueError:
-        pass
+def test_palindrome_raises_exception():
+    for test_case, expected in data_raises_exception:
+        try:
+            is_palindrome(test_case)
+        except ValueError as e:
+            assert e.args[0] == expected
+
+
+def test_palindrome_no_exception():
+    for test_case, expected in data_no_exception:
+        assert is_palindrome(test_case) == expected
 
 
 if __name__ == "__main__":
-    test_palindrome()
+    test_palindrome_no_exception()
+    test_palindrome_raises_exception()
